a6a7eb921853830b59af8d5be6c4dbdda69d91b3
cschwengel <crschwen@ucsc.edu>
2025-10-04 13:51:37 -0700

    Add files via upload

---
+++ Code-Journal-1.1.py
@@ -0,0 +1,12 @@
+#Connor Schwengel 10/4/2025
+
+
+def main():
+	print("Name: Connor Schwengel")
+	print("Prnouns: He/Him")
+	print("Favorite food: I dont eat a lot of different foods, so my choice will not be particularly creative. I enjoy garlic butter steak.")
+	print("Favorite movie: I enjoy a lot of different genres of movies so it is very difficult to pick a favorite. Recently, I would have to say \"Across the Spiderverse\"")
+
+
+if __name__ == '__main__':
+	main()
